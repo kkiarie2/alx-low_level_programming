20d9f1688c7b5926b4ff4e7b45fd6a14a6fa61a1
kiarie <kiariekelvin1@gmail.com>
2022-09-08 15:16:05 -0700

    sdf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(void)
+/*print some text*/
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+/*print some text*/
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
