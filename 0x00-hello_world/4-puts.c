8bdc7a1d1e8e027db169b3808c670df880ad939a
kiarie <kiariekelvin1@gmail.com>
2022-09-08 15:27:13 -0700

    jfds

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+ *prints text using puts
+ */
 int main(void)
-/*print some text*/
 {
-/*print some text*/
+/**
+*print some text
+*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
