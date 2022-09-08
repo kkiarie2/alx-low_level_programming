e48aa3f38baa1a10f0a0a1b3b111a322f0fd08bf
kiarie <kiariekelvin1@gmail.com>
2022-09-08 16:19:47 -0700

    sfe

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
 * main - entry point
 *
-* Return -Always 0.
+* Return - Always 0.
 *
 **/
-int main()
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
