3fbf46a9a3a83612f81e9b1602c0027804bb9d41
kiarie <kiariekelvin1@gmail.com>
2022-09-08 16:16:25 -0700

    jfkse

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return -Always 0.
+*
+**/
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
