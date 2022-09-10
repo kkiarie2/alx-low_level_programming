5b8b305892e32afb735c80234cf7809a97a4c328
kiarie <kiariekelvin1@gmail.com>
2022-09-10 07:52:09 -0700

    afeew

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: Always 0.
+ */
+int main(void)
+{
+char small;
+for (small = 'z'; small >= 'a'; small--)
+putchar(small);
+putchar("\n");
+return (0);
+}
