8d8b0b8398d944beb35a8fb2b5a2eae88192f00f
kiarie <kiariekelvin1@gmail.com>
2022-09-10 06:17:20 -0700

    fjsefe

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char small;
+char q= 'q';
+char e = 'e';
+for (small = 'a'; small <= 'z'; small++)
+if (small != e && small != q)
+putchar(small);
+printf("\n");
+return (0);
+}
