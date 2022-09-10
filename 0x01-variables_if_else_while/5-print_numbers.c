3210673be3ff2c51d1281f759e6ef41d25473c76
kiarie <kiariekelvin1@gmail.com>
2022-09-10 07:24:58 -0700

    ewfew

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,16 +3,13 @@
 #include <time.h>
 
 /**
-
  * main - entry point
-
  * Return: Always 0.
-
  */
 int main(void)
 {
 int num;
-for (num=0; num < 10; num++)
+for (num = 0; num < 10; num++)
 printf("%d", num);
 printf("\n");
 return (0);
