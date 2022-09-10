33560c44e488e71a2593c59827950af655ab4183
kiarie <kiariekelvin1@gmail.com>
2022-09-09 17:08:44 -0700

    afe

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -24,12 +24,12 @@ int main(void)
   
   n = rand() - RAND_MAX / 2;
 if(n == 0){
-printf("%lf is zero\n ", n);
+printf("%d is zero\n ", n);
 }else if (n > 0)
 {
-printf("%lf is positive\n", n);
+printf("%d is positive\n", n);
 }else{
-printf("lf is negative\n", n);
+printf("%d is negative\n", n);
 } 
    return (0);
   
