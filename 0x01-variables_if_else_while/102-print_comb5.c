9a7c2a6795545004264df952090f97eb44472550
kiarie <kiariekelvin1@gmail.com>
2022-09-12 08:35:57 -0700

    fesfes

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - entry point
+* Return: Always 0
+*/
+
+int main(void){
+
+int i, j, k, l;
+
+for (i = 0; i <= 9; i++)
+{
+
+for (j = 0; j <= 9; j++)
+{
+for (k = 0; k <= 9; k++)
+{
+for (l = k + 1; l <= 9; l++)
+{
+putchar((i % 10) + '0');
+
+putchar((j % 10) + '0');
+
+putchar(' ');
+
+putchar((k % 10) + '0');
+
+putchar((l % 10) + '0');
+
+if (i == 9 && j == 8 && k == 9 && l == 9)
+
+continue;
+
+putchar(',');
+
+putchar(' ');
+
+}
+
+}
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
+}
