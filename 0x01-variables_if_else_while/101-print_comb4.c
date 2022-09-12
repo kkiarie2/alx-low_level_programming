99a97331fbf6a1265d5519f5204987df4579bad7
kiarie <kiariekelvin1@gmail.com>
2022-09-12 06:58:31 -0700

    fewwfe:

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,46 +5,30 @@
 #include <time.h>
 
 /**
-
 * main - entry point
-
 * Return: Always 0
-
 */
 
-int main(void){
-
+int main(void)
+{
 int i, j, k;
 
-for (i = 0; i <= 9; i++) {
-
-for (j = i+1; j <= 9; j++) {
-
-for (k = j+1; k <= 9; k++)
-
+for (i = 0; i <= 9; i++)
 {
-
+for (j = i + 1; j <= 9; j++)
+{
+for (k = j + 1; k <= 9; k++)
+{
 putchar((i % 10) + '0');
-
 putchar((j % 10) + '0');
-
 putchar((k % 10) + '0');
-
 if (i == 7 && j == 8 && k == 9)
-
 continue;
-
 putchar(',');
-
 putchar(' ');
-
 }
-
 }
-
 }
-
 putchar('\n');
-
 return (0);
 }
