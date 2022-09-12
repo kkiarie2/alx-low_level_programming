4e7f3951589703255f9048dd382bb1e4cdb305ea
kiarie <kiariekelvin1@gmail.com>
2022-09-12 06:02:42 -0700

    fewfe

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,10 +7,12 @@
  * Return: Always 0
  */
 
-int main(void){
+int main(void)
+{
 int i, j;
-for (i = 0; i <= 9; i++) {
-for (j = i+1; j <= 9; j++) {
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
 {
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
@@ -18,7 +20,6 @@ if (i == 8 && j == 9)
 continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
