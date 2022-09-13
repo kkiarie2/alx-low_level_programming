2d5f681bdce991672af56366369a0ad7e71cad4c
kiarie <kiariekelvin1@gmail.com>
2022-09-13 08:50:10 -0700

    few

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
-* main - entry point
-* Return: Always 0
-*/
+ * main - entry point
+ * Return: Always 0
+ */
 
 int main (void){
 char *c = "_putchar";
 _putchar(*c);
-_putchar('\n')
+_putchar('\n');
 return (0);
 }
