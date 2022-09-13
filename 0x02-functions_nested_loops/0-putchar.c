730afa0a1c81cc53c15f0f0babc65af719a64810
kiarie <kiariekelvin1@gmail.com>
2022-09-13 16:27:52 -0700

    fwee

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,15 @@
  */
 
 int main (void){
-char *c = "_putchar";
-_putchar(*c);
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r'); 
 _putchar('\n');
 return (0);
 }
