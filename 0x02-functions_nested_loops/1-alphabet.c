cf55fc6fe7941cc66f6c425f2ab1dbd4be9fbf49
kiarie <kiariekelvin1@gmail.com>
2022-09-14 05:51:09 -0700

    ffewe

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* main - entry point
+* Return: Always 0
+*/
+
+int main (void)
+{
+
+char xy = 'a';
+
+while(xy <= 'z')
+
+{
+
+_putchar(xy);
+
+xy++;
+
+}
+_putchar('\n')
+return (0);
+}
