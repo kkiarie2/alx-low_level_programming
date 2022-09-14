c9307fcea6d86a66f9b8a31fc82cd0055e3b73e0
kiarie <kiariekelvin1@gmail.com>
2022-09-14 06:18:20 -0700

    fewew

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-*void print_alphabet- prints alphabet
-* Return: Always alphabet
-*/
+ * print_alphabet- prints alphabet
+ * Return: Always alphabet
+ */
 
 void print_alphabet(void)
 {
