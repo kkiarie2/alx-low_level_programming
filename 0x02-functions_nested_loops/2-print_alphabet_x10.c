8bdd7185c9b2df371a8d0bdedf7f6bcfc0aed261
kiarie <kiariekelvin1@gmail.com>
2022-09-14 06:47:41 -0700

    fewhjiggiul

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+* print_alphabet_x10
+* -prints a-z 10 times
+*/
+
+void print_alphabet_x10(void)
+
+{
+
+char xy = 'a';
+
+int i;
+
+for (i = 0; i <= 9; i++)
+
+{
+
+
+
+for (xy = 'a'; xy <= 'z'; xy++)
+
+{
+
+_putchar(xy);
+
+
+
+}
+
+_putchar('\n');
+
+}
+
+
+}
