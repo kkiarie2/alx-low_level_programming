268a38fb1c1dbbd8c00a8d423951e7828a173a1b
kiarie <kiariekelvin1@gmail.com>
2022-09-14 07:19:01 -0700

    hjkhuiehl

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
 
 
 /**
-* _islower - checks if char is lower
-* Return: prints 1 if char is lower and 0 if not
-*/
+ * _islower - checks if char is lower
+ * @c: character to check
+ * Return: prints 1 if char is lower and 0 if not
+ */
 
 int _islower(int c)
 {
