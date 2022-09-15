ce3fea6c17e39a899829e08942b4fca510fde2bd
kiarie <kiariekelvin1@gmail.com>
2022-09-15 16:46:38 -0700

    gkjhj

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+/**
+ * _isdigit -checks if character is 0-9
+ * @c: charater
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+
+{
+
+	if (c >= '0' && c <= '9')
+
+		return (1);
+
+	else
+
+		return (0);
+}
