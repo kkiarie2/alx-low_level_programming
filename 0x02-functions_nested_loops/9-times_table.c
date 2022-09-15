f6b9b028a32827ebd94c999aac3d9c1c4ba92310
kiarie <kiariekelvin1@gmail.com>
2022-09-14 17:51:11 -0700

    fewfw

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints table 9
+ * Return: always 0
+ */
+
+void print_times_table(void)
+
+{
+
+int i, j, prod;
+
+for(i=0; i<= 9; i++)
+
+{
+
+_putchar('0');
+
+for(j=1; j<= 9; j++)
+
+{
+_putchar(',');
+
+_putchar(' ');
+
+prod = i * j;
+
+if (prod  <= 9)
+
+_putchar(' ');
+
+else
+_putchar((prod % 10) + '0');
+
+}
+
+_putchar('\n');
+
+}
+}
+
+
