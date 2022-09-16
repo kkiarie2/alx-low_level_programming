c1a6dc27881b0d86f337fbb472552ff1e4422d55
kiarie <kiariekelvin1@gmail.com>
2022-09-15 17:00:58 -0700

    hhjk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+* mul - multiplies 2 numbers
+* @a: first number
+* @b: second number
+* Return: product
+*/
+
+
+
+int mul(int a, int b)
+
+{
+
+return (a * b);
+
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_numbers - prints 0-9
+* Return: 0
+*/
+
+void print_numbers(void)
+
+{
+
+int num;
+
+
+
+for (num = '0'; num <= '9'; num++)
+
+{
+
+_putchar(num);
+
+}
+
+_putchar('\n');
+}
