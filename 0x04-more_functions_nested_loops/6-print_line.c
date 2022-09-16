5b0c60922c132a4487f906a8c0c6ca68b56b1582
kiarie <kiariekelvin1@gmail.com>
2022-09-16 05:35:20 -0700

    hkh

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+
+/**
+* print_line - Prints lines
+* @n: number of lines
+* Return: void
+*/
+
+
+
+void print_line(int n)
+
+{
+
+int lines;
+
+
+
+if (n <= 0)
+
+{
+
+_putchar('\n');
+
+}
+
+else
+
+{
+
+for (lines = 1; lines <= n; lines++)
+
+{
+
+_putchar('_');
+
+}
+
+_putchar('\n');
+
+}
+
+}
