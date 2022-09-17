8a6a4c7954ba73186b5487189813d827c18e5bf1
kiarie <kiariekelvin1@gmail.com>
2022-09-16 17:07:22 -0700

    kjgkg

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,22 +12,25 @@ void print_square(int size)
 
 int row, col;
 
+if (size == 0)
+{
+_putchar('\n')
+}
+else
+{
+
 for (row = 0; row < size; row++)
 
 {
 
-
-
 for (col = 0; col < size; col++)
 
 {
-
 _putchar('#');
 
 }
 
-_putchar('\n');
-
 }
 
+}
 }
