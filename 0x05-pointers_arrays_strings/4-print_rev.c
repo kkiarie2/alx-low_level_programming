89001cc519fbde1e8454fc0ba8f86595f8230869
kiarie <kiariekelvin1@gmail.com>
2022-09-20 15:29:36 -0700

    hkjij

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,17 +2,18 @@
 
 /**
 * print_rev - reverses a string
-*
 * @s: string
+*
 */
+
 
 void print_rev(char *s)
 
 {
 
-int len;
+int len = 0;
 
-while (*(s + len) != '\0')
+while (s[len] != '\0')
 
 {
 
@@ -20,11 +21,14 @@ len++;
 
 }
 
-while (len--)
+len--;
+while (len >= 0)
 
 {
 
-_putchar(*(s + (len)));
+_putchar(s[len]);
+
+len--;
 
 }
 
