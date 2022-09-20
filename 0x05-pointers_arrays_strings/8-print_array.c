b3f7b3f3fb59ee2c77811a4606f849629e7f448e
kiarie <kiariekelvin1@gmail.com>
2022-09-20 16:52:32 -0700

    hkjhfd

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,25 +7,24 @@
 * @n: integer
 *
 */
-
 void print_array(int *a, int n)
-
 {
-int i;
-
-for (i = 0; i <= n - 1; i++)
+int i = 0;
+while (i < n)
 {
-if
-(a[i] < 0)
-a[i] = -a[i];
-if
-((i == (n - 1)))
-printf("%d", a[i]);
+if (i < (n - 1))
+{
+printf("%i, ", a[i]);
+}
 else
 {
-printf("%d, ", a[i]);
+printf("%i", a[i]);
 }
-_putchar('\n');
+
+i++;
+
 }
+
+printf("\n");
 
 }
