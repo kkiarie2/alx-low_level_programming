c65ef7b96f6cda8f3f402f0a8ecb62dde64deb94
kiarie <kiariekelvin1@gmail.com>
2022-09-20 13:27:08 -0700

    hlhghgk

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - this function does not modify n
+*
+* @s: string
+*
+* Return: nothing.
+*
+*/
+
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; ++i)
+return (i);
+}
