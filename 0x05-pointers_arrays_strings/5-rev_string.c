f081364deda58106f463bb4b5f6213be59f0abb9
kiarie <kiariekelvin1@gmail.com>
2022-09-20 14:34:25 -0700

    gkhgukg

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+* @s: string
+*
+*/
+
+
+
+void rev_string(char *s)
+{
+
+int i;
+
+int j;
+
+int tmp;
+
+i = 0;
+
+while (s[i] != '\0')
+
+{
+
+i++;
+
+}
+
+i = i - 1;
+
+j = 0;
+
+
+
+while (j < i)
+
+{
+
+tmp = s[i];
+
+s[i] = s[j];
+
+s[j] = tmp;
+
+j++;
+
+i--;
+
+}
+}
