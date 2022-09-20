241301973c69e97f3723a2f70a22d040ae38aa42
kiarie <kiariekelvin1@gmail.com>
2022-09-19 17:11:07 -0700

    hkhk

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -15,25 +15,3 @@ void reset_to_98(int *n)
 *n = 98;
 
 }
-
-/**
-* main - parameters are passed by value
-*
-* Return: Always 0.
-*/
-
-int main(void)
-
-{
-
-int n;
-
-int *p;
-
-n = 102;
-
-p = &n;
-
-reset_to_98(p);
-return (0);
-}
