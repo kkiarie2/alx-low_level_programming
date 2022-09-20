9864fe93287ea14dd960ddf462dc1fffd1d1b700
kiarie <kiariekelvin1@gmail.com>
2022-09-19 17:22:57 -0700

    hkjhjh

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+* swap_int - this function does not modify n
+*
+* @a: a useless integer
+*
+* @b: a useless integer
+*
+* Return: nothing.
+*
+*/
+
+void swap_int(int *a, int *b)
+
+{
+
+int c = *a;
+
+int d = *b;
+
+*a = d;
+
+*b = c;
+
+}
