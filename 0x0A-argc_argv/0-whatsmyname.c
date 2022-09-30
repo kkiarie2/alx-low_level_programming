25640159e61ee0c5c422bfdeb8ca993fabccc58f
kiarie <kiariekelvin1@gmail.com>
2022-09-30 06:03:51 -0700

    gkh

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* @argc: number of parameters
+* @argv: array of strings
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char **argv)
+
+{
+
+if (argc > 0)
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
