93e21bc5af2ad1039394048050a615c4b0ae9487
kiarie <kiariekelvin1@gmail.com>
2022-09-30 06:15:38 -0700

    gk

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* @argc: number of parameters
+* @argv: array of strings
+* Return: Always 0 (Success)
+*/
+
+
+
+int main(int argc, char **argv)
+
+{
+
+(void) argv;
+
+printf("%i\n", argc - 1);
+
+return (0);
+}
