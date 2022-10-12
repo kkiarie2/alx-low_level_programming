81f48fc78d6f5060014e6c67b42f7b4cdfaf2d06
kiarie <kiariekelvin1@gmail.com>
2022-10-11 18:38:15 -0700

    hkjhk

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+
+#include <stdlib.h>
+
+
+
+/**
+* init_dog - Entry point
+* @d: passing structure at the function
+* @name: store a string for main
+* @age: store a number for main
+* @owner: store a string for main
+
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+
+{
+
+/* Passing to stuct from main */
+
+if (d != NULL)
+
+{
+
+d->name = name;
+
+d->age = age;
+
+d->owner = owner;
+
+}
+}
